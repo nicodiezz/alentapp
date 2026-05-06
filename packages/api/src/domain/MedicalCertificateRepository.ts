ae872122befaed845a631008df5bd2a46e8c34a3
Santiago Andrada <santi200454@gmail.com>
2026-05-06 13:42:15 -0300

    feat(medicalCertificate): agrega funciones para invalidacion de certificados

--- packages/api/src/domain/MedicalCertificateRepository.ts
+++ packages/api/src/domain/MedicalCertificateRepository.ts
@@ -2,4 +2,6 @@ import { MedicalCertificateDTO, CreateMedicalCertificateRequest } from '@alentap
 
 export interface MedicalCertificateRepository {
     create(data: CreateMedicalCertificateRequest): Promise<MedicalCertificateDTO>;
+    findActiveByMemberId(memberId: string): Promise<MedicalCertificateDTO | null>;
+	invalidateById(id: string): Promise<void>;
 }
