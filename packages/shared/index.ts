dae305fba7de9dd0dafb918cc6864dd981ee44d8
FrancoArcee <franarce.2004@gmail.com>
2026-05-13 20:26:49 -0300

    feat(shared): agrega contratos para deportes

--- packages/shared/index.ts
+++ packages/shared/index.ts
@@ -31,3 +31,23 @@ export interface UpdateMemberRequest {
   category?: MemberCategory;
   status?: MemberStatus;
 }
+
+// ==========================================
+// Sport
+// ==========================================
+export interface SportDTO {
+  id: string; // UUID
+  name: string;
+  description: string;
+  max_capacity: number;
+  additional_price: number;
+  requires_medical_certificate: boolean;
+}
+
+export interface CreateSportRequest {
+  name: string;
+  description: string;
+  max_capacity: number;
+  additional_price: number;
+  requires_medical_certificate: boolean;
+}
