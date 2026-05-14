dc98aa5dc05a14b3f8910f84984718ee99943096
Santiago Andrada <santi200454@gmail.com>
2026-05-13 23:00:40 -0300

    feat(medicalCertificate): incorpora UpdateMedicalCertificatesUseCase

---
+++ packages/api/src/application/UpdateMedicalCertificateUseCase.ts
@@ -0,0 +1,34 @@
+import { MedicalCertificateRepository } from '../domain/MedicalCertificateRepository.js';
+import { MedicalCertificateValidator } from '../domain/services/MedicalCertificateValidator.js';
+import { MedicalCertificateDTO, UpdateMedicalCertificateRequest } from '@alentapp/shared';
+
+export class UpdateMedicalCertificateUseCase {
+    constructor(
+        private readonly medicalCertificateRepo: MedicalCertificateRepository,
+        private readonly medicalCertificateValidator: MedicalCertificateValidator
+    ) {}
+
+    async execute(id: string, data: UpdateMedicalCertificateRequest): Promise<MedicalCertificateDTO> {
+
+        //valida existencia del certificado
+        await this.medicalCertificateValidator.validateCertificateExists(id, this.medicalCertificateRepo);
+
+        //valida formato de fechas validas
+        if (data.issue_date) {
+            this.medicalCertificateValidator.validateDateFormat(data.issue_date);
+        }
+        if (data.expiry_date) {
+            this.medicalCertificateValidator.validateDateFormat(data.expiry_date);
+        }
+
+        //si se quiere modificar alguna fecha, obtiene el valor persistido de LA OTRA fecha y valida que expiry_date continue siendo posterior a issue_date.
+        if (data.issue_date || data.expiry_date) {
+            const existingCertificate = await this.medicalCertificateRepo.findById(id); //obtiene certificado
+            const issueDate = data.issue_date || existingCertificate!.issue_date; //obtiene valor de req si mandaron, sino: toma valor actual del certificado
+            const expiryDate = data.expiry_date || existingCertificate!.expiry_date;
+            this.medicalCertificateValidator.validateDates(issueDate, expiryDate); //delega validacion
+        }
+
+        return this.medicalCertificateRepo.update(id, data);
+    }
+}
