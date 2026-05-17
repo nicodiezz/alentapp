ed239b16fef81755283f1fae93e78b7b4e5af5f6
nicodiezz <nicolasagusdiez@gmail.com>
2026-05-17 16:53:10 -0300

    feat(web): crea las rutas de equipment loans

--- packages/web/src/Layout.tsx
+++ packages/web/src/Layout.tsx
@@ -83,6 +83,19 @@ function Layout() {
                                 Casilleros
                             </Text>
                         </RouterLink>
+
+                         <RouterLink to="/equipment-loans">
+                            <Text
+                                fontWeight="semibold"
+                                fontSize="sm"
+                                textTransform="uppercase"
+                                letterSpacing="wider"
+                                color="fg.muted"
+                                _hover={{ color: "blue.500", textDecoration: "none" }}
+                            >
+                                Equipamiento
+                            </Text>
+                        </RouterLink>
                         
                          <RouterLink to="/medical-certificates">
                             <Text 
