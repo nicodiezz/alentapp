1740d7b6cc7e4dac97a1441432900a33574f5bf8
Ramiro Gil <gilramiro49@gmail.com>
2026-05-31 10:29:46 -0300

    fix(payment): corrige mensaje de error en el caso de uso correspondiente a la modificación de un pago existente

--- packages/api/src/application/UpdatePaymentUseCase.ts
+++ packages/api/src/application/UpdatePaymentUseCase.ts
@@ -13,13 +13,13 @@ export class UpdatePaymentUseCase {
     async execute(id: string, data: UpdatePaymentRequest): Promise<PaymentDTO> {
         const existingPayment = await this.paymentRepo.findById(id);
         if (!existingPayment) {
-            throw new Error('El pago No existe');
+            throw new Error('No existe un pago con ese ID');
         }
 
         if (data.member_id) {
             const existingMember = await this.memberRepo.findById(data.member_id);
             if (!existingMember) {
-                throw new Error('El miembro No existe');
+                throw new Error('No existe un miembro con ese ID');
             }
         }
 
